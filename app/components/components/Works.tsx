5fce51d7b4371a970331ca03739d87900326e267
Vtolin <manusiamk14ebr@gmail.com>
2025-03-10 21:01:30 +0700

    Fixed ESLint errors and optimized images

--- app/components/components/Works.tsx
+++ app/components/components/Works.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const work = () => {
     return (
@@ -7,13 +8,13 @@ const work = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             
             <div className="bg-white p-6 rounded-lg shadow-md text-black w-[100%]" id="works">
-                <img src="/crud.png" className="pb-[20px]"/>
+                <Image src="/crud.png" alt="crud" className="pb-[20px]" />
                 <h3 className="text-xl font-semibold mb-2">Basic C.R.U.D. App with Node.Js</h3>
                 <p className="text-gray-600">A user management CRUD application built with Node.js and MySQL, allowing users to create, read, update, and delete records containing names, emails, and input dates.</p>
             </div>
 
             <div className="bg-white p-6 rounded-lg shadow-md w-[100%] text-black" id="works">
-                <img src="/porto.png" className="pb-[20px]"/>
+                <Image alt="portoapp" src="/porto.png" className="pb-[20px]"/>
                 <h3 className="text-xl font-semibold mb-2">Portofolio website with NextJs</h3>
                 <p className="text-gray-600">Project description goes here</p>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
     <div className="bg-gradient-to-b from-[#1e1b4b] to-[#312e81] h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold text-center my-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
-        Hi, I'm Rajendra Pasha
+      Hi, I&apos;m Rajendra Pasha
       </h1>
       <p className="text-center font-sans text-white">A junior Full Stack developer</p>
       <Btn />
